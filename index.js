8922bbe11d20fa84bc4145c8dd48984b30a662a0
José Ailton Júnior <107266472+joseailtoncjr@users.noreply.github.com>
2023-04-15 15:32:30 -0300

    Implementação do método GET.

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const usuarios = require('./routes/usuarios');
 const app = express();
 app.use(express.json());
 
+app.use('/usuarios', usuarios);
+
 app.listen(3000);
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const usuarios = express.Router();
+const fs = require('fs');
 
 usuarios.route('/')
-.get((req, res) => {})
-.post((req, res) => {})
-.put((req, res) => {})
-.delete((req, res) => {});
+
+.get((req, res) => {
+    res.json({mensagem: "GET realizado com sucesso"})
+})
+.post((req, res) => {
+    res.json({mensagem: "POST realizado com sucesso"})
+})
+.put((req, res) => {
+    res.json({mensagem: "PUT realizado com sucesso"})
+})
+.delete((req, res) => {
+    res.json({mensagem: "DELETE realizado com sucesso"})
+});
+
+function lerBancoDados(){ // função que retorna o banco de dados
+    const arquivo = fs.readFileSync('./db/db.json'); // leitura do arquivo
+    const db = JSON.parse(arquivo); // converte para objeto
+    return db;
+}
+
+function gravarBancoDados(db) { // grava o array modificado em formato json no arquivo db.json
+    fs.writeFileSync('./db/db.json', JSON.stringify(db));
+}
+
+
+
+
+
 
 
 module.exports = usuarios;
